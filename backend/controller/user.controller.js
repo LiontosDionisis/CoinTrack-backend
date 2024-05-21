12b7e7feb0a6e6ab3ad07962bd3a67909b0677e5
Rmoney <DionisisLiontos@outlook.com>
2024-05-21 04:34:11 +0300

    Added logger

--- backend/controller/user.controller.js
+++ backend/controller/user.controller.js
@@ -1,8 +1,10 @@
+const logger = require("../logger/logger");
 const User = require("../model/user.model");
 const {registerValidation, loginValidation} = require("./validation")
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
+logger = require("../logger/logger")
 
 
 const secretKey = process.env.JWT_SECRET;
@@ -10,6 +12,7 @@ const secretKey = process.env.JWT_SECRET;
 exports.updateEmail = async (req, res) => {
   const { error } = registerValidation(req.body);
   if (error) {
+      logger.error("Validation error: ", error);
       return res.status(400).json({ error: error.details[0].message });
   }
   const {userId, email} = req.body;
@@ -19,6 +22,7 @@ exports.updateEmail = async (req, res) => {
   const existingUser = await User.findOne({ email });
 
   if (existingUser && existingUser._id.toString() !== userId) {
+    logger.error("Email already registered: ", email);
     return res.status(409).json({ msg: "Email already registered" });
   }
 
@@ -29,7 +33,9 @@ exports.updateEmail = async (req, res) => {
     )
     console.log(email);
     res.status(200).json({msg: "Email updated!"});
+    logger.info("Email updated for user: ", userId)
   } catch (error) {
+    logger.error("Interlan server error: ", error)
     res.status(500).json({error: "Internal server error"});
   }
 }
@@ -37,6 +43,7 @@ exports.updateEmail = async (req, res) => {
 exports.updateName = async (req, res) => {
   const { userId, name } = req.body;
   if (!userId) {
+    logger.error("User ID was not retreived.");
     return res.status(400).json({ error: "User ID is required" });
   }
 
@@ -47,9 +54,9 @@ exports.updateName = async (req, res) => {
     );
 
     res.status(200).json({ name });
-    console.log("Name updated");
+    logger.info("Name updated: ", name);
   } catch (error) {
-    console.log("Error updating name:", error);
+    logger.info("Internal server error: ", error);
     res.status(500).json({ error: "Internal server error" });
   }
 };
@@ -57,6 +64,7 @@ exports.updateName = async (req, res) => {
 exports.updatePassword = async (req, res) => {
   const { error } = registerValidation(req.body);
   if (error) {
+      logger.error("Validation error: ", error)
       return res.status(400).json({ error: error.details[0].message });
   }
   const {userId, oldPass, newPass} = req.body;
@@ -64,11 +72,13 @@ exports.updatePassword = async (req, res) => {
   const user = await User.findOne({_id: userId});
 
   if (!userId) {
+    logger.error("User ID not retreived.")
     return res.status(400).json({ error: "User ID is required" });
   }
 
   const passwordMatch = await bcrypt.compare(oldPass, user.password);
   if (!passwordMatch) {
+    logger.error("Incorrect password for user with ID: ", userId);
     return res.status(401).json({error: "Incorrect password"});
   }
 
@@ -79,8 +89,9 @@ exports.updatePassword = async (req, res) => {
       {$set: {password: hashedPassword}}
     );
     res.status(200).json({msg: "Password updated"});
-    console.log("Password updated");
+    logger.info("Password updated for user with ID: ", userId)
   } catch (error) {
+    logger.error("Internal server error: ", error)
     res.status(500).json({error: "Internal server error"});
   }
 }
@@ -88,12 +99,14 @@ exports.updatePassword = async (req, res) => {
 exports.updateUsername = async(req, res) => {
   const { userId, username} = req.body;
   if (!userId) {
+    logger.error("User ID not retreived.")
     return res.status(404).json({error: "User ID not found"})
   }
 
   const existingUser = await User.findOne({ username });
 
   if (existingUser && existingUser._id.toString() !== userId) {
+    logger.error("Username already exists. Username: ", username)
     return res.status(409).json({ msg: "Username already registered" });
   }
 
@@ -104,10 +117,10 @@ exports.updateUsername = async(req, res) => {
     );
 
     res.status(200).json({username});
-    console.log("Username updated");
+    logger.info("Username updated: ", username);
   } catch (error) {
     res.status(500).json({error: "Internal server error"});
-    console.log("Error updating username, username already exists?");
+    logger.error("Internal server error: ", error);
   }
 }
 
@@ -118,15 +131,16 @@ exports.login = async (req, res) => {
   try {
     const user = await User.findOne({ username });
     if (!user) {
+      logger.error("Username not found: ", username);
       return res.status(404).json({ error: 'Username not found' });
     }
 
     const passwordMatch = await bcrypt.compare(password, user.password);
     if (!passwordMatch) {
+      logger.error("Incorrect password for username: ", username);
       return res.status(401).json({ error: 'Incorrect password' });
     }
 
-     // Generate a JWT token
      const token = jwt.sign({ userId: user._id }, secretKey, { expiresIn: '1h' });
 
      
@@ -138,10 +152,11 @@ exports.login = async (req, res) => {
    
   
     res.status(200).json({ token, name, username, totalIncome, totalExpenses, wallet});
+    logger.info("Login!:", username)
 
 
   } catch (error) {
-    console.error('Login failed:', error);
+    logger.error("Login failed: ", error);
     res.status(500).json({ error: 'Internal server error' });
   }
 };
@@ -151,17 +166,20 @@ exports.login = async (req, res) => {
 exports.create = async (req, res) => {
   const { error } = registerValidation(req.body);
   if (error) {
+      logger.error("Validation error: ", error);
       return res.status(409).json({ error: error.details[0].message });
   }
 
   try {
       const emailExists = await User.findOne({ email: req.body.email });
       if (emailExists) {
+          logger.error("Email already exists: ", email);
           return res.status(403).json({ error: 'Email already exists' });
       }
 
       const usernameExists = await User.findOne({ username: req.body.username });
       if (usernameExists) {
+          logger.error("Username already exists: ", username);
           return res.status(403).json({ error: 'Username already taken' });
       }
       
@@ -175,13 +193,15 @@ exports.create = async (req, res) => {
       });
 
       if(newUser.name === "") {
+        logger.error("Name is required.")
         return res.status(409).json({error: "Name is required"})
       }
 
       const savedUser = await newUser.save();
       res.status(200).json({ data: savedUser });
+      logger.info("User registered: ", username)
   } catch (err) {
-      console.error('Error creating user:', err);
+      logger.error("Internal server error: ", error)
       res.status(500).json({ error: 'Internal server error' });
   }
 };
@@ -193,11 +213,16 @@ exports.delete = async(req, res) => {
 
     try {
         const user= await User.findOne({_id: userId});
-        if(!user) return res.status(404).send("User not found")
+        if(!user){
+          logger.error("User not found: ", userId)
+          return res.status(404).send("User not found")
+        } 
+
         const result = await User.findOneAndDelete({_id: userId})
         res.status(200).json({data:result});
-        console.log("User deleted");
+        logger.info("User deleted with ID: ", userId)
     } catch(err) {
+        logger.error("Error: ", err);
         res.status(404).json({data: err});
     }
 }
